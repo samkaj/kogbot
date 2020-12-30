a6d7881634b7d90562daa97a184f8e2aa24ce38b
KajavaScript <kajavasamuel@gmail.com>
2020-12-30 14:48:57 +0100

    fix on_member_join

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import discord
 import json
 from discord.ext import commands
-intents = discord.Intents()
-intents.members = True
 
-client = commands.Bot(command_prefix="<", intents=intents)
+intent = discord.Intents(messages=True, members=True)
+
+client = commands.Bot(command_prefix="!", intents=intent)
 
 def load_from_data(filename):
     with open(f'data/{filename}.json', 'r', encoding='utf-8') as doc:
@@ -12,21 +12,31 @@ def load_from_data(filename):
 
 @client.command()
 async def ping(ctx):
+    print('pong')
     await ctx.send('pong')
 
-@client.command(pass_context=True)
-@commands.has_permissions(administrator=True)
-async def change_welcome_msg(ctx, *args):
+@client.command()
+async def set_default_channel(ctx, *args):
     data = load_from_data('messages')
-    data["DEFAULT_CHANNEL_ID"] = int(args)
+    data["DEFAULT_CHANNEL_ID"] = int(' '.join(args))
+    with open("data/messages.json", "w") as doc:
+        json.dump(data, doc)
+
+@client.command()
+async def set_welcome_message(ctx, *args):
+    data = load_from_data('messages')
+    data["WELCOME_MESSAGE"] = int(' '.join(args))
     with open("data/messages.json", "w") as doc:
         json.dump(data, doc)
     
-
 @client.event
 async def on_member_join(member):
     messages = load_from_data('messages')
-    await member.send(f"{messages['WELCOME_MESSAGE']} {messages['DEFAULT_CHANNEL_ID']}")
+    text_channel = messages['DEFAULT_CHANNEL_ID']
+    await member.send(f"{messages['WELCOME_MESSAGE']}\nBörja här: <#{text_channel}>!")
+
+
+
 
 print('Pierre-Bengt is online!')
 client.run(load_from_data('config')['DISCORD_TOKEN'])
