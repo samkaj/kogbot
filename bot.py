12077acc40b4ed968c92b69ebeb9321b34bd1697
samkaj <kajavasamuel@gmail.com>
2021-01-10 13:04:23 +0100

    mapped discord.Guild objects to respective GuildInfo object

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 if __name__ == '__main__':
     intent = discord.Intents(messages=True, members=True, guilds=True)
     bot = commands.Bot(command_prefix="!", intents=intent)
-    _guilds = []
+    guild_map = []
 
     #TODO: map guild objects with GuildInfo objects
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     async def on_ready():
         # create GuildInfo objects for all guilds
         for g in bot.guilds:
-            _guilds.append(guild_info.GuildInfo(g.id, g.channels[0].id))
+            guild_map.append((g, guild_info.GuildInfo(g.id, g.channels[0].id)))
 
     def load_from_data(filename):
         with open(f'data/{filename}.json', 'r', encoding='utf-8') as doc:
@@ -31,32 +31,29 @@ if __name__ == '__main__':
         text_channel = 'default channel here'
         welcome_message = 'default message'
         for g in _guilds:
-            if g.is_guild(g_id):
-                text_channel = g.get_msg_from_input('LANDING_PAGE_ID')
-                welcome_message = g.get_msg_from_input('WELCOME_MESSAGE')
-                landing_page = member.guild.get_channel(text_channel).mention # ???
-                await member.send(f"Jag heter Pierre-Bengt, och är en bot. Du gick nyss med i **{member.guild.name}**.\n{welcome_message}\nBörja här: {landing_page} :blue_heart:")
-                break
+            text_channel = g[1].get_msg_from_input('LANDING_PAGE_ID')
+            welcome_message = g[1].get_msg_from_input('WELCOME_MESSAGE')
+            landing_page = member.guild.get_channel(text_channel).mention # ???
+            await member.send(f"Jag heter Pierre-Bengt, och är en bot. Du gick nyss med i **{member.guild.name}**.\n{welcome_message}\nBörja här: {landing_page} :blue_heart:")
+            break
 
     @bot.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def new_welcome_message(ctx, *args):
         new_message = ' '.join(args)
-        for g in _guilds:
-            if g.is_guild(ctx.guild.id):
-                g.set_new_message('WELCOME_MESSAGE', new_message)
-                await ctx.send(f'I set the new welcome message to: \n"{new_message}"')
-                break
+        for g in guild_map:
+            g[1].set_new_message('WELCOME_MESSAGE', new_message)
+            await ctx.send(f'I set the new welcome message to: \n"{new_message}"')
+            break
 
     @bot.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def set_default_channel_id(ctx, arg):
         try:
             new_id = int(arg)
-            for g in _guilds:
-                if g.is_guild(ctx.guild.id):
-                    g.set_new_message('LANDING_PAGE_ID', new_id)
-                    await ctx.send(f'I set the new default channel ID to: \n"{ctx.guild.get_channel(new_id).mention}"')
+            for g in guild_map:
+                g[1].set_new_message('LANDING_PAGE_ID', new_id)
+                await ctx.send(f'I set the new default channel ID to: \n"{ctx.guild.get_channel(new_id).mention}"')
         except TypeError:
             await ctx.send(f'I could not set the new ID, try entering a number!')
 
